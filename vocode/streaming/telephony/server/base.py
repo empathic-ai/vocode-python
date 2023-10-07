beeaf745402183cbafe23be82b04d9115c0fbd6a
Catchawink <33239314+Catchawink@users.noreply.github.com>
2023-10-07 19:59:47 -0400

    Updated SMS demo.

--- vocode/streaming/telephony/server/base.py
+++ vocode/streaming/telephony/server/base.py
@@ -100,7 +100,7 @@ class TelephonyServer:
             )
 
         # twilio SMS endpoint
-        self.app.add_api_route("/sms", self.handle_sms, methods=["POST"])
+        self.router.add_api_route("/sms", self.handle_sms, methods=["POST"])
 
         # vonage requires an events endpoint
         self.router.add_api_route("/events", self.events, methods=["GET", "POST"])
